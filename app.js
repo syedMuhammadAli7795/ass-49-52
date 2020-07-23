7e588fa13fe71c8e6fc8dde27c2a79c436d53dd2
Syed Muhammad Ali <65005362+syedMuhammadAli7795@users.noreply.github.com>
2020-07-23 15:48:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+//..................................task2................................................
+// function para(){   
+// var para = document.getElementById('para');
+// par = " sit amet consectetur adipisicing elit. Molestias aut architecto mollitia repellat neque error esse assumenda porro voluptate?";
+// para.innerHTML = par;
+// }
+
+
+
+
+    
+}
